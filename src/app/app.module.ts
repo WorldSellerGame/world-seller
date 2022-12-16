5ca93990784ce00a709e4089c5c2fcbb3e781f55
Kyle Kemp <kyle@seiyria.com>
2022-12-15 21:54:36 -0600

    add ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,42 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsStoragePluginModule, StorageOption } from '@ngxs/storage-plugin';
+import { NgxsModule } from '@ngxs/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const Stores: any = {};
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const Migrations: any = {};
+
+const allStores = Object.keys(Stores).filter(x => x.includes('State')).map(x => Stores[x]);
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
-    enabled: !isDevMode(),
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    NgxsModule.forRoot(allStores, {
+      developmentMode: !isDevMode()
+    }),
+    NgxsLoggerPluginModule.forRoot(),
+    NgxsStoragePluginModule.forRoot({
+      migrations: Object.values(Migrations).flat() as any[],
+      storage: StorageOption.LocalStorage
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot()
+  ],
 
-    // Register the ServiceWorker as soon as the application is stable
-    // or after 30 seconds (whichever comes first).
-    registrationStrategy: 'registerWhenStable:30000'
-  })],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
