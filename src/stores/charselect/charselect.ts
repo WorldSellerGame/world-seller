f26be0546cabf903f96a6e08141a8d30360a2c21
Kyle Kemp <kyle@seiyria.com>
2022-12-22 18:56:32 -0600

    feat(ui): add notification for resource gain, closes #11

--- src/stores/charselect/charselect.ts
+++ src/stores/charselect/charselect.ts
@@ -1,9 +1,11 @@
 
 
 import { Injectable } from '@angular/core';
-import { Selector, State } from '@ngxs/store';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
 import { attachAction } from '@seiyria/ngxs-attach-action';
+import { NotifyService } from '../../app/services/notify.service';
 import { ICharSelect } from '../../interfaces';
+import { GainResources } from './charselect.actions';
 import { attachments } from './charselect.attachments';
 import { defaultCharSelect } from './charselect.functions';
 
@@ -14,7 +16,7 @@ import { defaultCharSelect } from './charselect.functions';
 @Injectable()
 export class CharSelectState {
 
-  constructor() {
+  constructor(private notifyService: NotifyService) {
     attachments.forEach(({ action, handler }) => {
       attachAction(CharSelectState, action, handler);
     });
@@ -33,6 +35,12 @@ export class CharSelectState {
   @Selector()
   static activeCharacterResources(state: ICharSelect) {
     return this.activeCharacter(state)?.resources ?? {};
+  }
+
+  @Action(GainResources)
+  async gainResources(ctx: StateContext<ICharSelect>, { resources }: GainResources) {
+    const resStr = Object.keys(resources).map(key => `${resources[key]}x ${key}`).join(', ');
+    this.notifyService.notify(`Gained ${resStr}!`);
   }
 
 }
