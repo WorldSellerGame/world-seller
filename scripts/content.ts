fb8e79478d54ee2ed8cc9db9ed829f0b58a0379f
Kyle Kemp <kyle@seiyria.com>
2022-12-23 21:23:53 -0600

    feat(ops): allow for separating resources and items into folders of files. closes #17

--- scripts/content.ts
+++ scripts/content.ts
@@ -1,3 +1,4 @@
+const { merge } = require('lodash');
 
 const path = require('path');
 const fs = require('fs-extra');
@@ -7,11 +8,24 @@ const yaml = require('js-yaml');
 const loadItems = async () => {
   fs.ensureDirSync('src/assets/content');
 
-  const files = await readdir('content/data');
-  files.forEach((file: any) => {
+  const files = await readdir('content/data', [(file: string, stats: any) => stats.isDirectory()]);
+  files.forEach((file: string) => {
     const data = yaml.load(fs.readFileSync(file, 'utf8'));
     fs.writeJson(`src/assets/content/${path.basename(file, '.yml')}.json`, data);
   });
+
+  await Promise.all(['items', 'resources'].map(async folder => {
+    const files = await readdir(`content/data/${folder}`);
+
+    const allData = {};
+
+    files.forEach((file: string) => {
+      const data = yaml.load(fs.readFileSync(file, 'utf8'));
+      merge(allData, data);
+    });
+
+    fs.writeJson(`src/assets/content/${folder}.json`, allData);
+  }));
 
   console.log('☑ Content loaded.');
 };
