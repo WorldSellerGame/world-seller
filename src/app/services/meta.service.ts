eb4eaf97cb0ea571e14ce27e72552b746426c2e4
jamspoon <jamspoonart@gmail.com>
2022-12-28 17:59:56 -0600

    change export extension to .ws

--- src/app/services/meta.service.ts
+++ src/app/services/meta.service.ts
@@ -36,7 +36,7 @@ export class MetaService {
         return acc;
       }, {} as any);
 
-      const fileName = `${charName}.qivan`;
+      const fileName = `${charName}.ws`;
       const dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(saveData));
       const downloadAnchorNode = document.createElement('a');
       downloadAnchorNode.setAttribute('href',     dataStr);
