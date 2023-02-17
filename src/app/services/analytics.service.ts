c340ec6580d7c4d5a567609958ea9d5b9e0eeab0
Kyle Kemp <kyle@seiyria.com>
2023-02-17 15:23:44 -0600

    whoops, add files

---
+++ src/app/services/analytics.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import gameanalytics from 'gameanalytics';
+import { environment } from '../../environments/environment';
+import { MetaService } from './meta.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalyticsService {
+
+  private analytics: typeof gameanalytics.GameAnalytics;
+
+  constructor(private metaService: MetaService) { }
+
+  init() {
+    this.analytics = gameanalytics.GameAnalytics;
+    this.analytics.setEnabledInfoLog(!environment.production);
+    this.analytics.configureBuild(`${environment.platform} ${this.metaService.version}`);
+    this.analytics.initialize(environment.gameanalytics.game, environment.gameanalytics.sdk);
+  }
+
+  sendDesignEvent(eventId: string, value: number = 0) {
+    this.analytics.addDesignEvent(eventId, value);
+  }
+}
