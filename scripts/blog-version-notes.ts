3697957b8f70bbe417baedef41e45f81e41e7a48
Kyle Kemp <kyle@seiyria.com>
2023-03-27 08:57:57 -0500

    fix(updates): blog post update file should be formatted correctly

--- scripts/blog-version-notes.ts
+++ scripts/blog-version-notes.ts
@@ -4,14 +4,13 @@ const fs = require('fs');
 const date = new Date();
 const year = date.getFullYear();
 const month = (date.getMonth() + 1).toString().padStart(2, '0');
-const day = date.getDay().toString().padStart(2, '0');
+const day = date.getDate().toString().padStart(2, '0');
 
 const hours = date.getHours().toString().padStart(2, '0');
 const minutes = date.getMinutes().toString().padStart(2, '0');
 const seconds = date.getSeconds().toString().padStart(2, '0');
 
-const header = `
----
+const header = `---
 title: Version Updates
 date: ${year}-${month}-${day} ${hours}:${minutes}:${seconds}
 categories: [New Version]
@@ -21,8 +20,7 @@ tags: [world-seller, version-notes]
 
 const changelog = fs.readFileSync('./CHANGELOG-current.md', 'utf8');
 
-const content = `
-${header}
+const content = `${header}
 
 ${changelog}
 `;
